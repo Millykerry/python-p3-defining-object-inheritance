73bbb058d6a6f9396085890281b3422d0448069f
millykerry <kerryom975@gmail.com>
2026-01-09 01:04:26 +0300

    Complete inheritance lab with Vehicle and Car classes

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,8 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    """Child class that inherits from Vehicle"""
+    
+    def go(self):
+        """Overwrites the inherited go() method with car-specific behavior"""
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
